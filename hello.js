c6d283fa63c3d307ab9b3b7a285e8381bee73c0a
charleenmperrier <charlee_perrier@hotmail.com>
2021-01-14 16:53:49 +0000

    practice

--- hello.js
+++ hello.js
@@ -7,3 +7,10 @@ let sayHi = function(name) {
   console.log("Hello " + name);
 }
 sayHi('Charlee')
+
+let returnHello = function(name) {
+  return 'Hello, ' + name;
+}
+let greeting = returnHello('charlee');
+console.log(greeting)
+

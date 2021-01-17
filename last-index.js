92de844bc5260f32c76b83cb4cd06d716cec5e74
charleenmperrier <charlee_perrier@hotmail.com>
2021-01-17 19:54:42 +0000

    very hard

---
+++ last-index.js
@@ -0,0 +1,35 @@
+const lastIndexOf = function (array, num) {
+  let indexNum = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === num) {
+      indexNum = i;
+    }
+  };
+  if (indexNum) {
+    return indexNum;
+  } else {
+    return -1;
+  }
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
+
+function lastIndexOf(series, value){
+  var foundMatch = false;
+  var result = 0;
+  for (var i = 0; i < series.length; i++) {
+    if(series[i] === value){
+      result = i;
+      foundMatch = true;
+  }
+  }
+  if (foundMatch){
+  return result;
+  } else {
+  return -1;
+  }
+  }
